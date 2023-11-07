3bc9964923a7abc2397c744230349a45b77eb76e
Qredo LTD <support@qredo.com>
2023-11-07 19:13:41 +0000

    Make protobuff generation script also create output directory

--- regen_go_protobufs.sh
+++ regen_go_protobufs.sh
@@ -1,1 +1,7 @@
-protoc --proto_path=protobuf --go_opt=Mpolicy.proto=github.com/qredo/blackbird/verifier/golang/protobuf --go_opt=module=github.com/qredo/blackbird/verifier/golang/protobuf --go_out=verifier/golang/protobuf protobuf/policy.proto
+mkdir -p verifier/golang/protobuf
+protoc \
+  --proto_path=protobuf \
+  --go_opt=Mpolicy.proto=github.com/qredo/blackbird/verifier/golang/protobuf \
+  --go_opt=module=github.com/qredo/blackbird/verifier/golang/protobuf \
+  --go_out=verifier/golang/protobuf \
+  protobuf/policy.proto
